7aafd3b227dd923b78f6c2d2772efb3a71170bbd
Konstantin Fomenkov <kolij2005@gmail.com>
2022-03-26 15:38:36 +0300

    models + data 0.6

--- MenuApp/Services/FoodData.swift
+++ MenuApp/Services/FoodData.swift
@@ -1,9 +1,9 @@
-//
-//  FoodData.swift
-//  MenuApp
-//
-//  Created by Konstantin Fomenkov on 25.03.2022.
-//
+    //
+    //  FoodData.swift
+    //  MenuApp
+    //
+    //  Created by Konstantin Fomenkov on 25.03.2022.
+    //
 
 import Foundation
 
@@ -16,9 +16,8 @@ class FoodData {
     let image = ["Борщ", "Солянка", "Куриный бульон", "Жаркое", "Плов", "Паста", "Медовик", "Сорбет", "Чай", "Кофе"]
     let calories = [40, 35, 29, 55, 65, 33, 60, 15, 5, 6]
     let price = [300, 290, 180, 380, 350, 240, 150, 100, 80, 90]
-    //давайте пока без ингредиентов, это массив в массиве получается, нам бы с этим справится!
-    //let ingredients: [[String]]
+        //давайте пока без ингредиентов, это массив в массиве получается, нам бы с этим справится!
+        //let ingredients: [[String]]
 
     private init() {}
-
 }
