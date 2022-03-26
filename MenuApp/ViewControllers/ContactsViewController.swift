47a0b75c7e379f3a335157ad90d5699c6b599f8a
Paul Matar <paul_matar@icloud.com>
2022-03-26 11:52:16 +0200

    ContactsVC StackView added

--- MenuApp/ViewControllers/ContactsViewController.swift
+++ MenuApp/ViewControllers/ContactsViewController.swift
@@ -9,13 +9,17 @@ import UIKit
 
 class ContactsViewController: UIViewController {
     
+    @IBOutlet var mapImage: UIImageView!
     var restaurant: Restaurant!
+    @IBOutlet var restaurantLabel: UILabel!
+    @IBOutlet var addressLabel: UILabel!
+    @IBOutlet var phoneLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        titleLabel.text = restaurant.title
-        adressLadel.text = restaurant.adress
-        phoneNumberLabel.text = "Tel: \(restaurant.numberPhone)"
+//        titleLabel.text = restaurant.title
+//        adressLadel.text = restaurant.adress
+//        phoneNumberLabel.text = "Tel: \(restaurant.numberPhone)"
     }
 }
