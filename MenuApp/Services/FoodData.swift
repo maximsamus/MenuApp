631573cc581807d6f645f045803f663c37150618
maximsamus <maximsamus1@gmail.com>
2022-03-27 13:41:52 +0400

    tea update

--- MenuApp/Services/FoodData.swift
+++ MenuApp/Services/FoodData.swift
@@ -13,7 +13,7 @@ class FoodData {
 
     let names = ["Борщ", "Солянка", "Куриный бульон", "Жаркое", "Плов", "Паста", "Медовик", "Сорбет", "Чай", "Кофе"]
 
-    let image = ["borsch", "solyanka", "chickensoup", "jarkoe", "plov", "pasta", "medovik", "sorbet", "Чай", "coffee"]
+    let image = ["borsch", "solyanka", "chickensoup", "jarkoe", "plov", "pasta", "medovik", "sorbet", "tea", "coffee"]
     let calories = [40, 35, 29, 55, 65, 33, 60, 15, 5, 6]
     let price = [300, 290, 180, 380, 350, 240, 150, 100, 80, 90]
 
